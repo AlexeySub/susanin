15713546bde9940cb94e0fde1fc4f09e77b9f2d4
AlexeySub <mr.317676@gmail.com>
2019-04-14 01:36:07 +0300

    aaaaaaaaaaaaaa

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('busstops/', views.BusStop.as_view()),
     path('busstops/<int:pk>/', views.BSDetail.as_view()),
     path('checkbus/', views.CheckBusStop.as_view()),
-    path('push/', views.PushNot.as_view()),
     path('way/', views.CreateWay.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -60,7 +60,11 @@ class CheckBusStop(views.View):
 class CreateWay(views.View):
     def post(self, request):
         data = parsers.JSONParser().parse(request)
-        print(data['text'])
-        way = mp.reduce_routes(data['text'])
-        print(way)
-        return http.HttpResponse('Ok')
+        print(data['txtway'])
+        data=data['txtway'].split('<br/>')
+        way = data[1] + '. ' + data[2] + '. '
+        data = data[3].split('</li><li>')
+        way = way + '. Пешком до остановки ' + data[0][39:].replace('\n', '') + ' ' + data[1].replace('\n', '') + 'Затем пешком ' + data[2].replace('</li></ul>', '')
+        way = way.replace(' км.', ' километров.')
+        way = way.replace(' м,', ' метров,')
+        return http.HttpResponse(renderers.JSONRenderer().render({'way':way.replace('мин.', 'минут')}))
