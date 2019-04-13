432812273a31745a724c843aae2c74bbab4ffdf4
AlexeySub <mr.317676@gmail.com>
2019-04-13 18:02:37 +0300

    3

--- api/models.py
+++ api/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class BusStop(models.Model):
     id = models.AutoField(primary_key=True, null=None, unique=True)
     busStopName = models.CharField(max_length=32)
-    latitude = models.DecimalField(max_digits=6, decimal_places=3)
-    longitude = models.DecimalField(max_digits=6, decimal_places=3)
+    latitude = models.DecimalField(max_digits=15, decimal_places=13)
+    longitude = models.DecimalField(max_digits=15, decimal_places=13)
